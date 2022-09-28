1666cb67defb895bb53c904831f28ff17ee0febb
fekrii <abdulrahman.fekrii@gmail.com>
2022-09-29 01:11:35 +0200

    adding static files settings

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,9 @@ FROM python:3.10
 
 WORKDIR /app
 COPY requirements.txt requirements.txt
-RUN pip install -r requirements.txt
+RUN pip install -r requirements.txt &&\
+    mkdir -p /vol/web/static && \
+    mkdir -p /vol/web/media
+
 
 
--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view as swagger_get_schema_view
@@ -23,3 +25,9 @@ urlpatterns = [
     path('swagger/schema/', schema_view.with_ui('swagger', cache_timeout=0), name="swagger-schema"),
     
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
