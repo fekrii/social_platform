b1b207a5bd2abc1ce1042d5af9f94bba6c0fb423
fekrii <abdulrahman.fekrii@gmail.com>
2022-09-28 15:25:56 +0200

    finsihing project

--- Dockerfile
+++ Dockerfile
@@ -5,15 +5,3 @@ COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
 
 
-#FROM python:3.10
-#
-#COPY . /app
-#WORKDIR /app
-#
-#RUN python3 -m venv /opt/venv
-#
-#RUN pip install pip --upgrade
-#RUN /opt/venv/bin/pip install -r requirements.txt
-
-#RUN chmod +x entrypoint.sh
-#CMD ["/app/entrypoint.sh"]
--- core/settings.py
+++ core/settings.py
@@ -21,12 +21,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = str(os.environ.get('SECRET_KEY'))
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = bool(int(os.environ.get('DEBUG'), 0))
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 
 
 # Application definition
